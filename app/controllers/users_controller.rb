644a156811c3108cecd81865165049476cceb87e
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-03 12:27:17 -0300

    Create endpoint for users creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      response = { id: user.id, email: user.email, message: 'Succesfully created' }
+      render json: response, status: :created
+    else
+      render json: user.errors
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firstName, :lastName, :email, :password)
+  end
+end
