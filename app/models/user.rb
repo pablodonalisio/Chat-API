39a3f8ecfc79fc4d9189f1c6e12501ef365aef47
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-03 13:57:59 -0300

    Change column names to snake_case in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :firstName, :lastName, :email, :password
+  validates_presence_of :first_name, :last_name, :email, :password
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_151628) do
+ActiveRecord::Schema.define(version: 2021_08_03_165510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "firstName"
-    t.string "lastName"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
