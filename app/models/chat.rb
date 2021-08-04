c538ec2bdd5a685b69db4c40f855555d1a35f993
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-04 19:36:15 -0300

    Validate if Chat already exists before creation

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,13 @@
 class Chat < ApplicationRecord
   validates :user_1_id, presence: true
   validates :user_2_id, presence: true
+  validate :chat_exists, on: :create
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
+
+  def chat_exists
+    chat = Chat.where(user_1_id: user_1_id, user_2_id: user_2_id)
+               .or(Chat.where(user_1_id: user_2_id, user_2_id: user_1_id))
+    errors.add(:chat, 'Already exists') if chat.any?
+  end
 end
