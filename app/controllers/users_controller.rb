31b9fd85d04d29fd0c5621c076c08f8747b5af71
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-04 15:10:41 -0300

    Set user method in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
     render json: user, status: :ok
   end
 
@@ -24,5 +23,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+  def user
+    @user ||= User.find(params[:id])
   end
 end
