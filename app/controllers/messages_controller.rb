6549a976a0faa4469eef314090c49d75004a5bd2
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-07 14:36:11 -0300

    Custmoize ActiveRecord::RecordNotFound response

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,13 @@
 class MessagesController < ApplicationController
-  before_action :set_chat
   before_action :user_is_in_chat
 
   def index
-    messages = @chat.messages.order(updated_at: :desc)
+    messages = chat.messages.order(updated_at: :desc)
     render json: messages, status: :ok
   end
 
   def create
-    message = @chat.messages.build(message_params)
+    message = chat.messages.build(message_params)
     message.user = @current_user
     if message.save
       render json: message, status: :created
@@ -19,9 +18,8 @@ class MessagesController < ApplicationController
 
   private
 
-  def set_chat
-    @chat ||= Chat.find_by(id: params[:chat_id])
-    render json: { error: "Couldn't find chat" }, status: :bad_request if @chat.nil?
+  def chat
+    @chat ||= Chat.find(params[:chat_id])
   end
 
   def message_params
@@ -30,6 +28,6 @@ class MessagesController < ApplicationController
 
   def user_is_in_chat
     error = { error: "You don't have access to this chat" }
-    render json: error, status: :forbidden unless @chat.users.include?(@current_user)
+    render json: error, status: :forbidden unless chat.users.include?(@current_user)
   end
 end
