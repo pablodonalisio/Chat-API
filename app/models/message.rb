cff22e6b8838f7f26e29620fd5e95b1bf5e4fd84
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-08 17:20:37 -0300

    Censor bad language when creating a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
+  include Censored
+
   belongs_to :user
   belongs_to :chat
+
+  before_save :censor_message
+
+  def censor_message
+    CENSORED_WORDS.each do |word|
+      body.gsub!(word) do
+        charset = Array('!'..'.')
+        Array.new(word.length) { charset.sample }.join
+      end
+    end
+  end
 end
