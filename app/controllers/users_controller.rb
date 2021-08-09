9fc4880eddb3f762692381796ef5530657552d48
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-09 19:44:28 -0300

    Allow to set chat settings when creating or updating User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :settings)
   end
 
   def user
