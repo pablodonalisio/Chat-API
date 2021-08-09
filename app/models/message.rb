418219ca45246ae89d8a91a8cb690fb43da9d938
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-09 14:48:53 -0300

    Solve merge conflicts

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,11 @@
 class Message < ApplicationRecord
+  include Censored
+
   belongs_to :user
   belongs_to :chat
 
   validate :last_message, on: :update
+  before_save :censor_message
 
   def last_message
     last_message_id = chat.messages
@@ -11,5 +14,14 @@ class Message < ApplicationRecord
                           .first
                           .id
     errors.add(:message, 'Can only edit last message') unless id == last_message_id
+  end
+
+  def censor_message
+    CENSORED_WORDS.each do |word|
+      body.gsub!(word) do
+        charset = Array('!'..'.')
+        Array.new(word.length) { charset.sample }.join
+      end
+    end
   end
 end
