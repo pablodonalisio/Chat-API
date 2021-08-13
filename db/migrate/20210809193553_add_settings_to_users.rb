70519071567dff9a134136b81ca7be0650df9977
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-08-13 11:20:45 -0300

    Define settings enum in rails, not in DB

--- db/migrate/20210809193553_add_settings_to_users.rb
+++ db/migrate/20210809193553_add_settings_to_users.rb
@@ -1,15 +1,5 @@
 class AddSettingsToUsers < ActiveRecord::Migration[6.0]
-  def up
-    execute <<-SQL
-      CREATE TYPE chat_settings AS ENUM ('all_upcase', 'all_downcase', 'no_accent_marks');
-    SQL
-    add_column :users, :settings, :chat_settings
-  end
-
-  def down
-    remove_column :users, :settings
-    execute <<-SQL
-      DROP TYPE chat_settings;
-    SQL
+  def change
+    add_column :users, :settings, :string
   end
 end
